da6ae3a44adcf8789f7624d051713f5acd2b698d
Daniel Barnhart <ing200086@gmail.com>
2020-05-20 00:31:56 -0500

    Updated to allow export of components.

--- src/main.js
+++ src/main.js
@@ -1,17 +1,8 @@
-import * as components from './components'
+import MyButton from './components/MyButton/MyButton'
+import MyBanner from './components/MyBanner/MyBanner'
 
-const ComponentLibrary = {
-    install(Vue) {
-        for (const componentName in components) {
-            const component = components[componentName]
+export default { MyBanner, MyButton }
 
-            Vue.component(component.name, component)
-        }
-    }
-}
-
-export default ComponentLibrary
-
-if (typeof window !== 'undefined' && window.Vue) {
-    window.Vue.use(ComponentLibrary)
-}
+// if (typeof window !== 'undefined' && window.Vue) {
+//     window.Vue.use(ComponentLibrary)
+// }
